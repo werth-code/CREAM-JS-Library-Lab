8cde854e6c63a1c65cb60e2a4bb8682165315929
Matt Werth <matthewwerth@gmail.com>
2021-01-20 14:46:52 -0500

    Added Tests For LinkedList

--- dictionary/Dictionary.js
+++ dictionary/Dictionary.js
@@ -1,5 +1,15 @@
-class Dictionary {
+function Dictionary(head) {
+    this.head = head
 
+    this.size = function() {
+        let count = 0; 
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }   
 }
 
 module.exports = Dictionary;
--- linkedList/LinkedList.test.js
+++ linkedList/LinkedList.test.js
@@ -1,25 +1,25 @@
 const { TestScheduler } = require('jest');
 const LinkedList = require('./LinkedList');
-const ListNode = require('./LinkedList');
+const ListNode = require('./ListNode');
 
+let node1;
+let node2;
+let list;
 
-test("Test Create List & Node", () => {
-    let node1 = new ListNode(10)
-    let node2 = new ListNode(20)
-    let list = new LinkedList(node1)
-    node1.next = node2
+beforeEach(() => {
+    node1 = new ListNode(10)
+    list = new LinkedList(node1)
+    node2 = new ListNode(20)
+});
 
-    let actual = list.data.data
+test("Create List With Head Node", () => {
+    let actual = list.head.data
     let expected = 10
 
     expect(actual).toEqual(expected);
 });
 
-test("Adding New Node", () => {
-
-    let node1 = new ListNode(10)
-    let node2 = new ListNode(20)
-    let list = new LinkedList(node1)
+test("Add New Node: .next", () => {
     node1.next = node2
 
     let actual = node1.next.data
@@ -28,13 +28,44 @@ test("Adding New Node", () => {
     expect(actual).toEqual(expected);
 });
 
-test("Get First Node getFirst()", () => {
-    let node1 = new ListNode(10)
-    let list = new LinkedList(node1)
-
-
-    let actual = list.getFirst()
+test("Get First Node: getFirst()", () => {
+    let actual = list.getFirst().data
     let expected = 10
+
+    expect(actual).toBe(expected);
+});
+
+test("Get Last Node: getLast()", () => {
+    node3 = new ListNode(30)
+    node1.next = node2
+    node2.next = node3
+
+    let actual = list.getLast().data
+    let expected = 30
+
+    expect(actual).toBe(expected);
+});
+
+test("Get List Size: size()", () => {
+    node3 = new ListNode(30)
+    node1.next = node2
+    node2.next = node3
+
+    let actual = list.size()
+    let expected = 3
+
+    expect(actual).toBe(expected);
+});
+
+test("Clear List: clear()", () => {
+    node3 = new ListNode(30)
+    node1.next = node2
+    node2.next = node3
+
+    list.clear()
+
+    let actual = list.size()
+    let expected = 0
 
     expect(actual).toBe(expected);
 });
