49b1a30a09ad0e5977138383f83be512cbfc5949
Matt Werth <matthewwerth@gmail.com>
2021-01-20 14:04:21 -0500

    Initial Commit

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -1,5 +1,43 @@
 class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
 
+    size() {
+        let count = 0; 
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }   
+
+    clear() {
+        this.head = null;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode
+    }
+}
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
 }
 
+
 module.exports = LinkedList;
+module.exports = ListNode;
--- linkedList/LinkedList.test.js
+++ linkedList/LinkedList.test.js
@@ -1,5 +1,40 @@
 const { TestScheduler } = require('jest');
 const LinkedList = require('./LinkedList');
+const ListNode = require('./LinkedList');
 
-test("First Test", () => {
+
+test("Test Create List & Node", () => {
+    let node1 = new ListNode(10)
+    let node2 = new ListNode(20)
+    let list = new LinkedList(node1)
+    node1.next = node2
+
+    let actual = list.data.data
+    let expected = 10
+
+    expect(actual).toEqual(expected);
+});
+
+test("Adding New Node", () => {
+
+    let node1 = new ListNode(10)
+    let node2 = new ListNode(20)
+    let list = new LinkedList(node1)
+    node1.next = node2
+
+    let actual = node1.next.data
+    let expected = 20
+
+    expect(actual).toEqual(expected);
+});
+
+test("Get First Node getFirst()", () => {
+    let node1 = new ListNode(10)
+    let list = new LinkedList(node1)
+
+
+    let actual = list.getFirst()
+    let expected = 10
+
+    expect(actual).toBe(expected);
 });
